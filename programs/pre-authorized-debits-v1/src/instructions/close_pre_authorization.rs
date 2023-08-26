d90b654c2bfea2468e7f7db53de3f647e3b9b6f7
matcha <matcha@dcaf.so>
2023-08-26 15:51:10 -0400

    chore(pre-authorized-debits-v1): remove unused accounts in IXs

--- programs/pre-authorized-debits-v1/src/instructions/close_pre_authorization.rs
+++ programs/pre-authorized-debits-v1/src/instructions/close_pre_authorization.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::TokenAccount;
 
-use crate::state::{pre_authorization::PreAuthorization, smart_delegate::SmartDelegate};
+use crate::state::pre_authorization::PreAuthorization;
 
 #[derive(Accounts)]
 pub struct ClosePreAuthorization<'info> {
@@ -16,20 +16,6 @@ pub struct ClosePreAuthorization<'info> {
     pub authority: Signer<'info>,
 
     pub token_account: Account<'info, TokenAccount>,
-
-    #[account(
-        mut,
-        seeds = [
-            b"smart-delegate",
-            token_account.key().as_ref(),
-        ],
-        bump = smart_delegate.bump,
-        // TODO: Throw custom error on failure
-        has_one = token_account,
-        // TODO: Throw custom error on failure
-        constraint = authority.key.eq(&token_account.owner) || authority.key.eq(&pre_authorization.debit_authority)
-    )]
-    pub smart_delegate: Account<'info, SmartDelegate>,
 
     #[account(
         mut,
--- programs/pre-authorized-debits-v1/src/instructions/init_pre_authorization.rs
+++ programs/pre-authorized-debits-v1/src/instructions/init_pre_authorization.rs
@@ -1,10 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::TokenAccount;
 
-use crate::state::{
-    pre_authorization::{PreAuthorization, PreAuthorizationVariant},
-    smart_delegate::SmartDelegate,
-};
+use crate::state::pre_authorization::{PreAuthorization, PreAuthorizationVariant};
 
 #[derive(Accounts)]
 #[instruction(params: InitPreAuthorizationParams)]
@@ -17,18 +14,6 @@ pub struct InitPreAuthorization<'info> {
     // TODO: Throw custom error on failure
     #[account(has_one = owner)]
     pub token_account: Account<'info, TokenAccount>,
-
-    #[account(
-        mut,
-        seeds = [
-            b"smart-delegate",
-            token_account.key().as_ref(),
-        ],
-        bump = smart_delegate.bump,
-        // TODO: Throw custom error on failure
-        has_one = token_account,
-    )]
-    pub smart_delegate: Account<'info, SmartDelegate>,
 
     #[account(
         init,
