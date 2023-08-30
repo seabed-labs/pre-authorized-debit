417156c44908925893bd92a8c24e13b15f597e92
Mocha Dcaf <mocha@dcaf.so>
2023-08-30 17:28:03 -0400

    chore(program-tests): lint

--- program-tests/pre-authorized-debit-v1/pre-authorized-debit-v1.ts
+++ program-tests/pre-authorized-debit-v1/pre-authorized-debit-v1.ts
@@ -3,13 +3,13 @@ import { expect } from "chai";
 import { PreAuthorizedDebitV1 } from "../../target/types/pre_authorized_debit_v1";
 
 describe("pre-authorized-debit-v1", () => {
-    // Configure the client to use the local cluster.
-    anchor.setProvider(anchor.AnchorProvider.env());
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
 
-    const program = anchor.workspace
-        .SmartAccountV1 as anchor.Program<PreAuthorizedDebitV1>;
+  const program = anchor.workspace
+    .SmartAccountV1 as anchor.Program<PreAuthorizedDebitV1>;
 
-    it("empty test", () => {
-        expect(1).to.equal(1);
-    });
+  it("empty test", () => {
+    expect(1).to.equal(1);
+  });
 });
