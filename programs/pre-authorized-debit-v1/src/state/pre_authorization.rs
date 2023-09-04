8b00d5ab9456deb9b569890f0565b0b656cbccca
Mocha Dcaf <mocha@dcaf.so>
2023-09-04 12:50:54 -0400

    docs: add comments to pre_authorization state

--- programs/pre-authorized-debit-v1/src/state/pre_authorization.rs
+++ programs/pre-authorized-debit-v1/src/state/pre_authorization.rs
@@ -4,12 +4,45 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(Default, InitSpace)]
 pub struct PreAuthorization {
-    pub paused: bool,
-    pub token_account: Pubkey,
-    pub variant: PreAuthorizationVariant,
-    pub debit_authority: Pubkey,
-    pub activation_unix_timestamp: i64,
+    /**
+      The `bump` is the canonical PDA bump when derived with seeds:
+      ['pre-authorization', token_account, debit_authority].
+      This field is initialized in `init_pre_authorization`.
+      This field is never updated in any instruction.
+    */
     pub bump: u8,
+    /**
+      If `paused === true`, then the `debit_authority` cannot debit via the `token_account`.
+      This field is initialized to `false` in `init_pre_authorization`.
+      This field can be updated by the `token_account.owner` in `update_pause_pre_authorization`.
+    */
+    pub paused: bool,
+    /**
+      The `token_account` is the account the `debit_authority` will be able to debit from.
+      This field is initialized in `init_pre_authorization`.
+      This field is never updated in any instruction.
+    */
+    pub token_account: Pubkey,
+    /**
+      The `variant` contains the data specific to a one-time
+      or recurring debit.
+      This field is initialized in `init_pre_authorization`.
+      This field is never updated in any instruction.
+    */
+    pub variant: PreAuthorizationVariant,
+    /**
+      The `debit_authority` is the key that can debit from the `token_account`.
+      This field is initialized in `init_pre_authorization`.
+      This field is never updated in any instruction.
+    */
+    pub debit_authority: Pubkey,
+    /**
+      The `activation_unix_timestamp` represents when the debit_authority can next debit
+      from the `token_account`.
+      The field is initialized to __ in `init_pre_authorization`.
+      This field is updated in __ TODO(18f6ba).
+    */
+    pub activation_unix_timestamp: i64,
 }
 
 #[derive(InitSpace, AnchorSerialize, AnchorDeserialize, Clone)]
