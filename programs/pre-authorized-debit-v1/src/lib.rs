206c93f510aa42e5774a00ff59e606ad906a9737
Mocha Dcaf <mocha@dcaf.so>
2023-09-04 14:32:59 -0400

    docs: add comments for close_smart_delegate

--- programs/pre-authorized-debit-v1/src/lib.rs
+++ programs/pre-authorized-debit-v1/src/lib.rs
@@ -13,7 +13,7 @@ pub mod pre_authorized_debit_v1 {
     use super::*;
 
     /**
-    The `init_smart_delegate` instruction will create a `smart_delegate` account.
+    The `InitSmartDelegate` instruction will create a `smart_delegate` account.
 
     Initializes a new account (`smart_delegate`).
     The `token_account.delegate` is set to the newly created `smart_delegate` account.
@@ -28,7 +28,7 @@ pub mod pre_authorized_debit_v1 {
     The `token_program` MUST be either the token program or token 22 program.
     The `system_program` MUST be the system program.
 
-      Accounts expected by this instruction:
+    Accounts expected by this instruction:
       0. `[writable]` payer: The payer for the `smart_delegate`.
       1. `[]`         owner: The new accounts owner.
       2. `[writable]` token_account: The `token_account` this `smart_delegate` will sign for as the `token_account.delegate`.
@@ -40,7 +40,26 @@ pub mod pre_authorized_debit_v1 {
         handle_init_smart_delegate(ctx)
     }
 
+    /**
+    The `CloseSmartDelegate` instruction will create close a `smart_delegate` account.
 
+    Closes an existing `smart_delegate` account.
+    The token program `revoke` instruction will be called on the `token_account`.
+
+    The `CloseSmartDelegate` instruction requires the `owner` to sign the transaction.
+    The `receiver` and `owner` may be the same account.
+    The `owner` MUST be the `token_account.owner`.
+    The `token_account.owner` MUST be the `owner`.
+    The `smart_delegate.token_account` must be the same as `token_account`.
+    The `token_program` MUST be either the token program or token 22 program.
+
+    Accounts expected by this instruction:
+      0. `[writable]` receiver: The receiver of the `smart_delegate` lamports.
+      1. `[]`         owner: The `token_account.owner`.
+      2. `[writable]` token_account: The `token_account` associated to the `smart_delegate` being closed.
+      3. `[writable]` smart_delegate.
+      4. `[]`         token_program.
+    */
     pub fn close_smart_delegate(ctx: Context<CloseSmartDelegate>) -> Result<()> {
         handle_close_smart_delegate(ctx)
     }
