91710de72edc2752321a0392fc433672f1657290
Mocha Dcaf <mocha@dcaf.so>
2023-09-04 13:22:33 -0400

    docs: add comments to pad states

--- programs/pre-authorized-debit-v1/src/state/smart_delegate.rs
+++ programs/pre-authorized-debit-v1/src/state/smart_delegate.rs
@@ -3,7 +3,24 @@ use anchor_lang::prelude::*;
 // PDA Seeds: ['smart-delegate', token_account]
 #[account]
 #[derive(Default, InitSpace)]
+/**
+  The `smart_delegate` is a PDA account derived with the seeds:
+  ['smart-delegate', token_account].
+  The `smart_delegate` is set as the delegate of
+  the `token_account` in `init_smart_delegate` with u64::max.
+  A `smart_delegate` is associated 1:1 with a `token_account`.
+*/
 pub struct SmartDelegate {
-    pub token_account: Pubkey,
+    /**
+      The `bump` is the canonical PDA bump when derived with seeds:
+      ['smart-delegate', token_account].
+      This field is initialized in `init_smart_delegate`.
+      This field is never updated in any instruction.
+    */
     pub bump: u8,
+    /**
+      The `token_account` is initialized in `init_smart_delegate`.
+      This field is never updated in any instruction.
+    */
+    pub token_account: Pubkey,
 }
