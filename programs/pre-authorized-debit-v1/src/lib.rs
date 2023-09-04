8a9f344681a07dfcb375921cf31da0f844591da0
Mocha Dcaf <mocha@dcaf.so>
2023-09-04 14:44:08 -0400

    docs: add comments for init_pre_authorization

--- programs/pre-authorized-debit-v1/src/lib.rs
+++ programs/pre-authorized-debit-v1/src/lib.rs
@@ -26,7 +26,6 @@ pub mod pre_authorized_debit_v1 {
     The `owner` MUST be the `token_account.owner`.
     The `payer` and `owner` may be the same account.
     The `token_program` MUST be either the token program or token 22 program.
-    The `system_program` MUST be the system program.
 
     Accounts expected by this instruction:
       0. `[writable]` payer: The payer for the `smart_delegate`.
@@ -64,6 +63,32 @@ pub mod pre_authorized_debit_v1 {
         handle_close_smart_delegate(ctx)
     }
 
+    /**
+    The `InitPreAuthorization` instruction will create a `pre_authorization` account.
+
+    Initializes a new account (`pre_authorization`).
+    The `pre_authorization` defines a set of rules.
+    The `pre_authorization` rules/constraints are verified during a `debit` instruction.
+    The `pre_authorization` in conjunction with a `smart_delegate` for the same `token_account`
+    can allow the `pre_authorization.debit_authority` to do a one-time or recurring debit from the
+    `token_account.
+    For a pair of `debit_authority` and `token_account`, only a single `pre_authorization` account can exist.
+    To create another `pre_authorization` for the same `token_account`, another `debit_authority` must be used.
+
+    The `InitPreAuthorization` instruction requires the `payer` and `owner` to sign the transaction.
+    The `payer` MUST sign for the instruction and have enough lamports to pay for the `pre_authorization` account.
+    The `owner` MUST be the `token_account.owner`.
+    The `payer` and `owner` may be the same account.
+    The `token_account.owner` MUST be the `owner`.
+    The `pre_authorization.token_account` must be the same as `token_account`.
+
+    Accounts expected by this instruction:
+      0. `[writable]` payer: The payer for the `pre_authorization` account.
+      1. `[]`         owner: The `token_account.owner`.
+      2. `[writable]` token_account: The `token_account` associated to the `pre_authorization` being created.
+      3. `[writable]` pre_authorization: The `pre_authorization` is the new account being initialized.
+      4. `[]`         system_program.
+    */
     pub fn init_pre_authorization(
         ctx: Context<InitPreAuthorization>,
         params: InitPreAuthorizationParams,
