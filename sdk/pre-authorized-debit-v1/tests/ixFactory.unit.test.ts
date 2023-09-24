58055d4de1d2aed43b5caabbd0505b8eba25e1bc
Mocha Dcaf <mocha@dcaf.so>
2023-09-24 17:25:14 -0400

    test: buildInitOneTimePreAuthorizationIx

--- sdk/pre-authorized-debit-v1/tests/ixFactory.unit.test.ts
+++ sdk/pre-authorized-debit-v1/tests/ixFactory.unit.test.ts
@@ -2,13 +2,16 @@ import { InstructionFactoryImpl } from "../src/write/implementation";
 import { Connection, Keypair, SystemProgram } from "@solana/web3.js";
 import {
   InitOneTimePreAuthorizationParams,
+  InitRecurringPreAuthorizationParams,
   PreAuthorizedDebitReadClientImpl,
 } from "../src";
 import * as sdkConstants from "../src/constants";
 import { expect } from "chai";
 import { createSandbox } from "sinon";
+import { BorshCoder } from "@coral-xyz/anchor";
+import { IDL } from "@dcaf/pad-test-utils/pre_authorized_debit_v1";
 
-describe.only("InstructionFactory Unit Tests", () => {
+describe("InstructionFactory Unit Tests", () => {
   const sandbox = createSandbox();
 
   const connection: Connection = new Connection("http://my.rpc");
@@ -21,6 +24,8 @@ describe.only("InstructionFactory Unit Tests", () => {
     sdkConstants.MAINNET_PAD_PROGRAM_ID,
     readClient,
   );
+
+  const coder = new BorshCoder(IDL);
 
   afterEach(() => {
     sandbox.reset();
@@ -97,6 +102,127 @@ describe.only("InstructionFactory Unit Tests", () => {
 
       const ix =
         await instructionFactory.buildInitOneTimePreAuthorizationIx(params);
+
+      expect(ix.instruction.keys[0].pubkey.toString()).to.equal(
+        params.payer.toString(),
+      );
+      expect(ix.instruction.keys[0].isSigner).to.equal(true);
+      expect(ix.instruction.keys[0].isWritable).to.equal(true);
+
+      expect(ix.instruction.keys[1].pubkey.toString()).to.equal(
+        mockTokenAccountOwner.toString(),
+      );
+      expect(ix.instruction.keys[1].isSigner).to.equal(true);
+      expect(ix.instruction.keys[1].isWritable).to.equal(false);
+
+      expect(ix.instruction.keys[2].pubkey.toString()).to.equal(
+        smartDelegate.publicKey.toString(),
+      );
+      expect(ix.instruction.keys[2].isSigner).to.equal(false);
+      expect(ix.instruction.keys[2].isWritable).to.equal(false);
+
+      expect(ix.instruction.keys[3].pubkey.toString()).to.equal(
+        params.tokenAccount.toString(),
+      );
+      expect(ix.instruction.keys[3].isSigner).to.equal(false);
+      expect(ix.instruction.keys[3].isWritable).to.equal(true);
+
+      expect(ix.instruction.keys[4].pubkey.toString()).to.equal(
+        preAuthorization.toString(),
+      );
+      expect(ix.instruction.keys[4].isSigner).to.equal(false);
+      expect(ix.instruction.keys[4].isWritable).to.equal(true);
+
+      expect(ix.instruction.keys[5].pubkey.toString()).to.equal(
+        mockTokenProgramId.toString(),
+      );
+      expect(ix.instruction.keys[5].isSigner).to.equal(false);
+      expect(ix.instruction.keys[5].isWritable).to.equal(false);
+
+      expect(ix.instruction.keys[6].pubkey.toString()).to.equal(
+        SystemProgram.programId.toString(),
+      );
+      expect(ix.instruction.keys[6].isSigner).to.equal(false);
+      expect(ix.instruction.keys[6].isWritable).to.equal(false);
+
+      expect(ix.expectedSigners.length).to.equal(2);
+      expect(ix.expectedSigners[0].publicKey.toString()).to.equal(
+        params.payer.toString(),
+      );
+      expect(ix.expectedSigners[0].reason).to.equal(
+        "The 'payer' account needs to sign to pay for the creation of the pre-authorization account",
+      );
+      expect(ix.expectedSigners[1].publicKey.toString()).to.equal(
+        mockTokenAccountOwner.toString(),
+      );
+      expect(ix.expectedSigners[1].reason).to.equal(
+        "The 'owner' (i.e. token account's owner) needs to sign to create a pre-authorization for a token account",
+      );
+
+      expect(Object.keys(ix.meta).length).to.equal(1);
+      expect(ix.meta.preAuthorization.toString()).to.equal(
+        preAuthorization.toString(),
+      );
+
+      expect(
+        stubFetchCurrentOwnerOfTokenAccount.calledOnceWith(params.tokenAccount),
+      ).to.equal(true);
+      expect(
+        stubFetchTokenProgramIdForTokenAccount.calledOnceWith(
+          params.tokenAccount,
+        ),
+      ).to.equal(true);
+    });
+  });
+
+  context("buildInitRecurringPreAuthorizationIx", () => {
+    it("should build init pre authorization ix for recurring pad", async () => {
+      const params: InitRecurringPreAuthorizationParams = {
+        payer: Keypair.generate().publicKey,
+        tokenAccount: Keypair.generate().publicKey,
+        debitAuthority: Keypair.generate().publicKey,
+        activation: new Date(),
+        repeatFrequencySeconds: BigInt(60),
+        recurringAmountAuthorized: BigInt(99),
+        resetEveryCycle: false,
+        numCycles: null,
+      };
+      const smartDelegate = readClient.getSmartDelegatePDA();
+      const mockTokenAccountOwner = Keypair.generate().publicKey;
+      const mockTokenProgramId = Keypair.generate().publicKey;
+      const preAuthorization = readClient.derivePreAuthorizationPDA(
+        params.tokenAccount,
+        params.debitAuthority,
+      ).publicKey;
+
+      const stubFetchCurrentOwnerOfTokenAccount = sandbox
+        .stub(readClient, "fetchCurrentOwnerOfTokenAccount")
+        .resolves(mockTokenAccountOwner);
+      const stubFetchTokenProgramIdForTokenAccount = sandbox
+        .stub(readClient, "fetchTokenProgramIdForTokenAccount")
+        .resolves(mockTokenProgramId);
+
+      const ix =
+        await instructionFactory.buildInitRecurringPreAuthorizationIx(params);
+
+      const ixData =
+        // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+        (coder.instruction.decode(ix.instruction.data)?.data as any).params;
+      expect(Object.keys(ixData).length).to.equal(3);
+      expect(ixData.debitAuthority.toString()).to.equal(
+        params.debitAuthority.toString(),
+      );
+      expect(ixData.activationUnixTimestamp.toString()).to.equal(
+        Math.floor(params.activation.getTime() / 1e3).toString(),
+      );
+      expect(
+        ixData.variant.recurring.repeatFrequencySeconds.toString(),
+      ).to.equal(params.repeatFrequencySeconds.toString());
+      expect(ixData.variant.recurring.resetEveryCycle).to.equal(false);
+      expect(
+        ixData.variant.recurring.recurringAmountAuthorized.toString(),
+      ).to.equal(params.recurringAmountAuthorized.toString());
+      expect(ixData.variant.recurring.numCycles).to.equal(null);
 
       expect(ix.instruction.keys[0].pubkey.toString()).to.equal(
         params.payer.toString(),
