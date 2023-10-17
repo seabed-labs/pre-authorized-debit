49337e05260fd1262ba30f3ea45ef489e247b831
matcha <matcha@dcaf.so>
2023-10-17 15:18:55 -0400

    fix: build error

--- web/src/components/Chakra.tsx
+++ web/src/components/Chakra.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 // e.g. src/Chakra.js
 // a) import `ChakraProvider` component as well as the storageManagers
 import { ChakraProvider, cookieStorageManagerSSR, localStorageManager } from '@chakra-ui/react';
 import theme from '../styles/theme';
+import { PropsWithChildren } from 'react';
 
-export function Chakra({ cookies, children }: any) {
+export function Chakra({ cookies, children }: PropsWithChildren<{ cookies: object }>) {
     // b) Pass `colorModeManager` prop
     const colorModeManager = typeof cookies === 'string' ? cookieStorageManagerSSR(cookies) : localStorageManager;
 
